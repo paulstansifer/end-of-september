be3e51c8017401e1d473af943570c90ed4a5d4c6
Daniel Klein <othercriteria@daniel-kleins-macbook.local>
2008-07-14 14:41:38 -0400

    Final clean-up.

--- misc/icfp08/src/driver.py
+++ misc/icfp08/src/driver.py
@@ -10,10 +10,6 @@ def dturn(s):
           'r': soft_turn,
           'R': hard_turn}[s]
 
-#TODO: calculate momentum and figure it out
-def r_inertia():
-  return .2
-
 def move(turn, urgency=1):
   if turn > 180: turn -= 360
   elif turn < -180: turn += 360
@@ -30,13 +26,14 @@ def move(turn, urgency=1):
     elif abs(turn)*(1+urgency) < 65:
       comm += 'a'
 
-  if turn_state == 'R' and turn < hard_turn * r_inertia():
+  r_inertia = 0.2
+  if turn_state == 'R' and turn < hard_turn * r_inertia:
     comm += 'l'
-  elif turn_state == 'L' and turn > -hard_turn * r_inertia():
+  elif turn_state == 'L' and turn > -hard_turn * r_inertia:
     comm += 'r'
-  elif turn_state == 'r' and turn < soft_turn * r_inertia():
+  elif turn_state == 'r' and turn < soft_turn * r_inertia:
     comm += 'l'
-  elif turn_state == 'l' and turn > -soft_turn * r_inertia():
+  elif turn_state == 'l' and turn > -soft_turn * r_inertia:
     comm += 'r'
   elif turn < -soft_turn / 2:
     comm += 'l'
@@ -89,55 +86,48 @@ turn_state = '-'
 move_state = '-'
 next_heading = 0
 turning_momentum = 0
-run = 0
 
 # Event loop
-while 1:
-  # Ignore all but most recent message; all we really care about is T anyways
-  # (the last thing from split is the empty message after the ';')
+while True:
   data = s.recv(1024).split(';')[0].split(' ')
-  print data
-  
-  if data[0] == 'T':
-    last_heading = heading
-    ares.set_time(float(data[1]) / 1000)
-    move_state = data[2][0]
-    turn_state = data[2][1]
-    x = float(data[3])
-    y = float(data[4])
-    heading = float(data[5])
-    speed = float(data[6])
 
-    turning_momentum = heading - last_heading
-    next_heading = heading + turning_momentum
-    ##TODO: figure out rotational acclr
-    
-    i = 7
-    while i+1 < len(data):
-      obj_type = data[i]
-      if obj_type == 'b' or obj_type == 'c':
-        ares.rec_boulder( (float(data[i+1]), float(data[i+2])),
-                     float(data[i+3]))
-        i += 4
-      elif obj_type == 'm':
-        ares.rec_martian( (float(data[i+1]), float(data[i+2])),
-                     float(data[i+3]), float(data[i+4]))
-        i += 5
-      elif obj_type == 'h':
-        #we already know where it is!
-        i += 4
-      elif obj_type == ';':
-        break;
-      else:
-        print "AIIIIEEE [" + obj_type + "]"
-        sys.exit()
-    #process_repulsion(ares.total_repel((x,y)))
-    process_occlusion(ares.total_occulsion( (x,y), heading))
-  elif data[0] == 'E':
+  if not data[0] in ('T', 'E'):
+    print data
+    continue
+
+  if data[0] == 'E':
     ares.martians = []
-    run += 1
-    if run >= 5:
-      s.close()
+    continue
+
+  last_heading = heading
+  ares.set_time(float(data[1]) / 1000)
+  move_state = data[2][0]
+  turn_state = data[2][1]
+  x = float(data[3])
+  y = float(data[4])
+  heading = float(data[5])
+  speed = float(data[6])
+  
+  turning_momentum = heading - last_heading
+  next_heading = heading + turning_momentum
+    
+  i = 7
+  while i+1 < len(data):
+    obj_type = data[i]
+    if obj_type == 'b' or obj_type == 'c':
+      ares.rec_boulder( (float(data[i+1]), float(data[i+2])),
+                        float(data[i+3]))
+      i += 4
+    elif obj_type == 'm':
+      ares.rec_martian( (float(data[i+1]), float(data[i+2])),
+                        float(data[i+3]), float(data[i+4]))
+      i += 5
+    elif obj_type == 'h':
+      #we already know where it is!
+      i += 4
+    elif obj_type == ';':
+      break;
+    else:
+      print "AIIIIEEE [" + obj_type + "]"
       sys.exit()
-  #else:
-  #  print data
+  process_occlusion(ares.total_occulsion( (x,y), heading))
