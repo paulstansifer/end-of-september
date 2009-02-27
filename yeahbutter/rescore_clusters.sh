10ddd677dfb48cbf68b15f912cc3af3d55e49550
Paul Stansifer <paul.stansifer@gmail.com>
2009-02-27 00:12:57 -0500

    Okay, on these settings, the model's actual results are bonkers, but look at this: > rmsep(lm(ebert ~ mc, data=test)) [1] 0.6166564 > > rmsep(lm(ebert ~ nf, data=test)) [1] 0.8608464 > rmsep(lm(ebert ~ nf+yb, data=test)) [1] 0.7664548 > rmsep(lm(ebert ~ yb, data=test)) [1] 0.9180437
    yb is driving the results down a significant portion of the way to
    metacritic's predictive power.  This is interesting.  Again, if you
    look at the actual results, you'll see that the best movie of all time
    is by Prince ("Purple Rain"?), and the second-best film is a
    "medicore" action film with a 9% rating at Rotten Tomatoes.  (Hm.  I
    need to throw some trashy movies into the dataset.)  But it seems like
    it's picking up something.  No idea what.

--- yeahbutter/rescore_clusters.sh
+++ yeahbutter/rescore_clusters.sh
@@ -1,7 +1,7 @@
 YB=/Users/paul/src/yeahbut/
 
 echo scoring...
-$YB/build/cpp/score -p 0.50 -c cluster_assignments -v ex.users_* | sort -n | tee temp_scores_inbetween |  $YB/yeahbutter/recover_netflix_titles.pl ex.map_articles ../../movie_titles.txt ../average_scores /dev/stdin | sort -n > temp_scores
+$YB/build/cpp/score -p 1.0 -c cluster_assignments -v ex.users_* | sort -n | tee temp_scores_inbetween |  $YB/yeahbutter/recover_netflix_titles.pl ex.map_articles ../../movie_titles.txt ../average_scores /dev/stdin | sort -n > temp_scores
 
 echo rating...
 $YB/yeahbutter/rate.pl --yb_scores temp_scores --ratings $YB/yeahbutter/ratings.txt --source ebert --source mc --source ord_nf --source ord_yb > i_e_m_n_y.matrix
